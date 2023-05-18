3dde52ad6dc56c72edce7ef6c0fca6ba0f2a116c
Kristian <popovichkristian@gmail.com>
2023-05-18 19:47:42 +0300

    jQuery Section - finished

---
+++ jQuery/index.js
@@ -0,0 +1,35 @@
+$('h1').css('color', 'red')
+
+console.log($('img').attr('src'))
+
+$('a').attr('href', 'https://www.yahoo.com')
+
+//Adding Event Listeners with jQuery
+$('h1').click(function () {
+	$('h1').css('color', 'purple')
+})
+
+$('button').click(function () {
+	$('h1').css('color', 'purple')
+})
+
+$(document).keypress(function (event) {
+	$('h1').text(event.key)
+})
+
+$('h1').on('mousover', function () {
+	$('h1').css('color', 'purple')
+})
+
+// Adding and Removing Elements with jQuery
+// $('h1').before('<button>New</button>')
+// $('h1').after('<button>New</button>')
+// $('h1').prepend('<button>New</button>')
+// $('h1').append('<button>New</button>')
+
+// $('button').remove()
+
+// Website Animations with jQuery
+$('button').on('click', function () {
+	$('h1').fadeToggle()
+})
